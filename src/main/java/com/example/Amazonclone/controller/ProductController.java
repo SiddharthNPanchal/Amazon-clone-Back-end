1457eba788444e96f7fff3f198977007f7b7a718
SiddharthNPanchal <panchalsid570@gmail.com>
2021-04-17 09:31:52 -0400

    Updated Code

--- src/main/java/com/example/Amazonclone/controller/ProductController.java
+++ src/main/java/com/example/Amazonclone/controller/ProductController.java
@@ -6,16 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.Amazonclone.service.ProductService;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class ProductController {
-
 
     @Autowired
     private ProductService service;
@@ -34,5 +34,61 @@ public class ProductController {
 
         service.insertProducts(product);
         return  new ResponseEntity(product, HttpStatus.OK);
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity getCategories(){
+//        System.out.println("Hello");
+        var cust = new customizedResponse("A list of all categories", service.getCategories());
+//        service.getCategories();
+        return new ResponseEntity(cust, HttpStatus.OK);
+    }
+
+    @GetMapping("/product-category/{category}")
+    public ResponseEntity getProductsfromCat(@PathVariable String category){
+//        System.out.println("Hello");
+        var cust = new customizedResponse("A list of all Products by category", service.getProductsfromCat(category));
+//      service.getCategories();
+        return new ResponseEntity(cust, HttpStatus.OK);
+    }
+
+    @GetMapping("/products/bestsellers")
+    public ResponseEntity getBestSellers(){
+//        System.out.println("Hello");
+        var cust = new customizedResponse("A list of all Bestsellers", service.getBestSellers());
+
+        return new ResponseEntity(cust, HttpStatus.OK);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity getProduct(@PathVariable String id){
+//        System.out.println("Hello");
+        //List<Product> temp = Collections.singletonList(service.getProduct(id));
+        var cust = new customizedResponse("A list of all Products by category", Collections.singletonList(service.getProduct(id)));
+//      service.getCategories();
+        System.out.println(cust.getBody());
+        if(cust.getBody().get(0)== Optional.empty()){
+            return new ResponseEntity(cust, HttpStatus.OK);
+        }
+        else{
+            System.out.println("Not found!");
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity updateProduct(@PathVariable String id, @RequestBody Product p){
+//        System.out.println("Hello");
+        var cust = new customizedResponse("Updated Product", Collections.singletonList(service.updateProduct(id, p)));
+//      service.getCategories();
+        return new ResponseEntity(cust, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity deleteAProduct(@PathVariable String id)
+    {
+        service.deleteAProduct(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/example/Amazonclone/service/ProductService.java
+++ src/main/java/com/example/Amazonclone/service/ProductService.java
@@ -3,9 +3,11 @@ package com.example.Amazonclone.service;
 import com.example.Amazonclone.model.Product;
 import com.example.Amazonclone.model.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ProductService {
@@ -16,9 +18,65 @@ public class ProductService {
     public List<Product> getProducts(){
         return repo.findAll();
     }
-
     public void insertProducts(Product product){
         repo.insert(product);
+    }
+
+    public List<String> getCategories(){
+//        System.out.println(repo.findAll());
+        Set<String> cat= new HashSet<>();
+        for(Product p: repo.findAll()){
+
+            cat.add(p.getCategory());
+        }
+        List<String> targetList = List.copyOf(cat);
+        return targetList;
+    }
+
+    public List<Product> getProductsfromCat(String category){
+//        List<Product> products = repo.findAll();
+        List<Product> products = new ArrayList<>();
+        for(Product p: repo.findAll()){
+            if(p.getCategory().equals(category)){
+                products.add(p);
+            }
+        }
+        return products;
+    }
+
+    public List<Product> getBestSellers(){
+//        List<Product> products = repo.findAll();
+        List<Product> products = new ArrayList<>();
+        for(Product p: repo.findAll()){
+            if(p.isBestSeller()){
+                products.add(p);
+            }
+        }
+        return products;
+    }
+
+    public Optional<Product> getProduct(String id)
+    {
+        return repo.findById(id);
+    }
+
+    public Product updateProduct(String id, Product p){
+        Optional<Product> ProductData = repo.findById(id);
+        if (ProductData.isPresent()) {
+            Product setData = ProductData.get();
+            setData.setPrice(p.getPrice());
+            setData.setQty(p.getQty());
+            return repo.save(setData);
+        }
+        else{
+            return null;
+        }
+
+    }
+
+    public void deleteAProduct(String id)
+    {
+        repo.deleteById(id);
     }
 
 }
